0d7b10342109845d5722bc6b58ff035edc5d880d
Unnikrishnan R <unnik@btech.au.dk>
2022-12-30 14:34:24 +0100

    Added readme, and animation for speed primer using coroutines.

--- Assets/ExperimentManagerScript.cs
+++ Assets/ExperimentManagerScript.cs
@@ -15,6 +15,7 @@ public class ExperimentManagerScript : MonoBehaviour
         fraction = 0;
         //anchorsLst = new List<GameObject>();
         StartCoroutine(MoveRing());
+        //StartCoroutine(MoveFromTo(ring.transform,anchorsLst[1].transform, anchorsLst[2].transform, 0.01f));
     }
 
     // Update is called once per frame
@@ -23,7 +24,24 @@ public class ExperimentManagerScript : MonoBehaviour
         
     }
 
+    //Write a coroutine to iterate through a list of gameobjects, call the MoveFromTo function on each pair of gameobjects 
+    //and wait for the coroutine to finish before moving to the next pair of gameobjects
     IEnumerator MoveRing()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(MoveFromTo(ring.transform, anchorsLst[listPos].transform, anchorsLst[listPos + 1].transform, 0.1f));            
+            if (listPos < anchorsLst.Count - 2)
+            {
+                listPos++;                
+            }
+            else
+                break;
+        }
+    }
+
+
+    /*IEnumerator MoveRing()
     { 
         while (listPos < anchorsLst.Count-1)
         {
@@ -40,6 +58,21 @@ public class ExperimentManagerScript : MonoBehaviour
             }
         }
         
+    }*/
+    
+    IEnumerator MoveFromTo(Transform objectToMove, Transform a, Transform b, float speed) //Adapted from https://gamedev.stackexchange.com/questions/100535/coroutine-to-move-to-position-passing-the-movement-speed
+    {
+        float step = (speed / (a.position - b.position).magnitude) * Time.fixedDeltaTime;
+        float t = 0;
+        while (t <= 1.0f)
+        {
+            t += step; // Goes from 0 to 1, incrementing by step each time
+            objectToMove.position = Vector3.Lerp(a.position, b.position, t); // Move objectToMove closer to b
+            objectToMove.rotation = Quaternion.Lerp(a.rotation, b.rotation, t);
+            yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+        }
+        objectToMove.position = b.position;
+        objectToMove.rotation = b.rotation;
     }
 
 }
